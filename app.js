96e15a54e299e327ddc6d6962c0827a1f108662f
Sameer727873 <111059349+Sameer727873@users.noreply.github.com>
2022-10-07 04:34:14 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// function readmore(){
+//     var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis voluptatum ex pariatur harum similique labore ipsa quo ut neque odit eius reiciendis et sunt, ipsum veritatis, iste asperiores cum doloremque!"
+//     var peragraf = document.getElementById("para")
+//     peragraf.innerHTML = text
+// }
+
+function prc(){
+    var a = document.getElementById("mg")
+    a.className = "car"
+    a.src = "https://wallpaperaccess.com/full/51367.jpg"
+}
+
+function prc2(){
+    var b = document.getElementById("mg")
+    b.className = "car2"
+    b.src = "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__480.jpg"
+
+}
+
+// var a = document.getElementsByTagName("p");
+// a[2].style.color = "red"
